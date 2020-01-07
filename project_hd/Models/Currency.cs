62c7fb9bc2e63b7bd9342b15cee74bcfa7251243
Konrad <konrad.flaka@gmail.com>
2020-01-07 19:30:40 +0100

    load data from db and asing to window object on the index page

---
+++ project_hd/Models/Currency.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace project_hd.Models
+{
+    [Serializable]
+    public class Currency
+    {
+        public Currency() { }
+
+        public static Currency CreateNew(dynamic obj)
+        {
+            var cur = new Currency();
+            cur.Bpi = obj.bpi;
+            cur.Disclaimer = obj.disclaimer;
+            cur.Time = obj.time;
+
+            return cur;
+        }
+
+        [BsonId]
+        public string CurrencyId { get; set; }
+
+        [JsonProperty]
+        public dynamic Time { get; set; }
+        [JsonProperty]
+        public string Disclaimer { get; set; }
+        [JsonProperty]
+        public dynamic Bpi { get; set; }
+    }
+}
