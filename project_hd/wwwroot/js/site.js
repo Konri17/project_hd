48c60d53ab89e28743b980df6898c37a04eb339a
Kamil689 <kamilkoterla@gmail.com>
2020-01-07 19:49:56 +0100

    implement basic frontend

--- project_hd/wwwroot/js/site.js
+++ project_hd/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('btnE').addEventListener("click", function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            alert(xhr.response);
+        }
+    };
+    xhr.open('get', '/HOME/insertapi', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send();
+});
+
+document.getElementById('btnT').addEventListener("click", function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            alert(xhr.response);
+        }
+    };
+    xhr.open('get', '/HOME/ReadObject', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send();
+});
+
+document.getElementById('btnDropDB').addEventListener("click", function(){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            alert(xhr.response);
+        }
+    };
+    xhr.open('get', '/HOME/CLEARDB', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send();
+});
